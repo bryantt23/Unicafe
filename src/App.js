31bf95507897e477a49df847ef119d57c1f3993c
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-19 19:44:47 -0800

    Calculate statistics

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+function App() {
   return (
-    <div>
-      code here
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-)
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(5);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const all = good + neutral + bad;
+  const positive = (good / all) * 100 + '%';
+  const average = (good - bad) / all;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <button
+          onClick={() => {
+            setGood(good + 1);
+          }}
+        >
+          good
+        </button>
+        <button
+          onClick={() => {
+            setNeutral(neutral + 1);
+          }}
+        >
+          neutral
+        </button>
+        <button
+          onClick={() => {
+            setBad(bad + 1);
+          }}
+        >
+          bad
+        </button>
+      </div>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
